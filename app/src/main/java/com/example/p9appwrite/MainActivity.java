da06ffd8c6ea03da9e703ad329af412f1770a008
davidelpuig <david@elpuig.xeill.net>
2025-01-08 15:59:53 +0100

    Account session

--- app/src/main/java/com/example/p9appwrite/MainActivity.java
+++ app/src/main/java/com/example/p9appwrite/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.p9appwrite;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import io.appwrite.Client;
+import io.appwrite.coroutines.CoroutineCallback;
 import io.appwrite.services.Account;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,5 +27,20 @@ public class MainActivity extends AppCompatActivity {
 
         Client client = new Client(getApplicationContext());
         client.setProject("677e7a6a003b07cd04f3");
+
+        Account account = new Account(client);
+
+        account.createEmailPasswordSession(
+                "a@a.com", // email
+                "Hola_1234", // password
+                new CoroutineCallback<>((result, error) -> {
+                    if (error != null) {
+                        error.printStackTrace();
+                        return;
+                    }
+
+                    Log.d("Appwrite", result.toString());
+                })
+        );
     }
 }
